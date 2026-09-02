17ebdd98296df00f2d743ef34fad24c2205be645
ferraricastro <filipecastrosaraiva@gmail.com>
2026-09-02 10:53:08 -0300

    update: sidebard functions

--- src/components/sidebar/sidebar-content.tsx
+++ src/components/sidebar/sidebar-content.tsx
@@ -1,7 +1,79 @@
+'use client';
+
+import {
+  Plus as AddIcon,
+  ArrowLeftToLine,
+  ArrowRightToLine,
+  X as CloseButton,
+} from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Button } from '../ui/button';
+import { Logo } from '../logo';
+
 export const SidebarContent = () => {
+  const router = useRouter();
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const collapsedSidebar = () => setIsCollapsed(true);
+  const expandSidebar = () => setIsCollapsed(false);
+
+  const handleNewPrompt = () => router.push('/new');
   return (
-    <aside>
-      <h2></h2>
+    <aside
+      className={`border-r border-gray-700 felx flex-col h-full bg-gray-800 transition-[transform, width] duration-300 ease-in-out fixed md:relative left-0 top-0 z-50 md:z-auto w-[80vw] sm:w-[320px] ${isCollapsed ? 'md:w-18' : 'md:w-87'}`}
+    >
+      {isCollapsed && (
+        <section className="px-2 py-6">
+          <header className="flex items-center justify-center mb-6">
+            <Button
+              onClick={expandSidebar}
+              variant="icon"
+              className="hidden md:inline-flex p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-accent-500 rounded-lg transition-colors"
+              aria-label="Expandir side bar"
+              title="Expandir side bar"
+            >
+              <ArrowRightToLine className="w-5 h-5 text-gray-100" />
+            </Button>
+          </header>
+        </section>
+      )}
+
+      {!isCollapsed && (
+        <>
+          <section className="p-6">
+            <div className="md:hidden mb-4">
+              <div className="flex items-center justify-between">
+                <Button
+                  variant="secondary"
+                  aria-label="Fechar menu"
+                  title="Fechar menu"
+                >
+                  <CloseButton className="w-5 h-5 text-gray-100" />
+                </Button>
+              </div>
+            </div>
+            <div className="flex w-full items-center justify-between mb-6">
+              <header className="flex w-full items-center justify-between">
+                <Logo />
+                <Button
+                  onClick={collapsedSidebar}
+                  variant="icon"
+                  className="hidden md:inline-flex p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-accent-500 rounded-lg transition-colors"
+                >
+                  <ArrowLeftToLine className="w-5 h-5 text-gray-100" />
+                </Button>
+              </header>
+            </div>
+            <div>
+              <Button onClick={handleNewPrompt} className="w-full" size="lg">
+                <AddIcon className="w-5 h-5 mr-2" />
+                Novo Prompt
+              </Button>
+            </div>
+          </section>
+        </>
+      )}
     </aside>
   );
 };

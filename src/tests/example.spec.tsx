3b64d9e25a737358d8d85cb99266af121dc37810
ferraricastro <filipecastrosaraiva@gmail.com>
2026-09-02 15:33:32 -0300

    add: first test

--- src/tests/example.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@/lib/test-utils';
-
-describe('Example Test', () => {
-  it('should pass', () => {
-    render(<div>Teste</div>);
-
-    expect(screen.getByText('Teste')).toBeInTheDocument();
-  });
-});

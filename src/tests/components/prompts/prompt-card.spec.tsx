8b363ed335a8a5099b8d6d2da20ee3a00b0d91c9
ferraricastro <filipecastrosaraiva@gmail.com>
2026-09-04 06:39:04 -0300

    update: delete test

--- src/tests/components/prompts/prompt-card.spec.tsx
+++ src/tests/components/prompts/prompt-card.spec.tsx
@@ -1,20 +1,32 @@
 import {
   PromptCard,
   type PromptCardProps,
-} from '@/components/prompts/prompts-card';
+} from '@/components/prompts/prompt-card';
 import { render, screen } from '@/lib/test-utils';
+import userEvent from '@testing-library/user-event';
 
 const makeSut = ({ prompt }: PromptCardProps) => {
   return render(<PromptCard prompt={prompt} />);
 };
 
 describe('PromptCard', () => {
+  const user = userEvent.setup();
+  const prompt = { id: '1', title: 'title 01', content: 'content 01' };
+
   it('deveria renderizar o link com href corretamente', async () => {
-    const prompt = { id: '1', title: 'title 01', content: 'content 01' };
     makeSut({ prompt });
     const link = screen.getByRole('link');
 
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', `/${prompt.id}`);
+  });
+
+  it('deveria abrir o dialog de remoção de um prompt', async () => {
+    makeSut({ prompt });
+
+    const deleteButton = screen.getByRole('button', { name: 'Remover Prompt' });
+    await user.click(deleteButton);
+
+    expect(screen.getByText('Remover Prompt')).toBeInTheDocument();
   });
 });

e728e63bb108c464f1aabfe70b06afafe808bb2f
ferraricastro <filipecastrosaraiva@gmail.com>
2026-09-04 01:35:49 -0300

    add: suspense on return SidebarContent

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,6 +2,8 @@ import { PromptSummary } from '@/core/domain/prompts/prompt.entity';
 import { PrismaPromptRepository } from '@/infra/repository/prisma-prompt.repository';
 import { prisma } from '@/lib/prisma';
 import { SidebarContent } from './sidebar-content';
+import { Suspense } from 'react';
+import { Spinner } from '../ui/spinner';
 
 export const Sidebar = async () => {
   const repository = new PrismaPromptRepository(prisma);
@@ -16,5 +18,9 @@ export const Sidebar = async () => {
     initialPrompts = [];
   }
 
-  return <SidebarContent prompts={initialPrompts} />;
+  return (
+    <Suspense fallback={<Spinner />}>
+      <SidebarContent prompts={initialPrompts} />
+    </Suspense>
+  );
 };
